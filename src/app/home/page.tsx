703d6dcbd01a3fe8a6a3e10e3fd0d74811f9ea7d
daguttt <dagutmu667@gmail.com>
2024-09-18 11:26:38 -0500

    feat: add service to fetch student last seen classes and add transfomer layer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,19 @@
 import { cookies } from 'next/headers';
+import { getStudentLastSeenClasses } from '~/services/classes/last-seen-classes';
 
-export default function HomePage() {
+export default async function HomePage() {
   const cookiesStore = cookies();
   const token = cookiesStore.get('AUTH_TOKEN');
+
+  const { failureRes, successRes } = await getStudentLastSeenClasses();
+  if (failureRes) return <p>{failureRes.detail}</p>;
 
   if (!token) return <p>Token not found</p>;
 
   return (
     <main className="container mx-auto flex min-h-[100svh] items-center justify-center px-4">
       <h1>Bienvenido</h1>
+      <pre>{JSON.stringify(successRes, null, 2)}</pre>
     </main>
   );
 }
