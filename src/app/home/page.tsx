4a82b750e7c6fdc97cc9da7c83dcc976d2903e90
Hernan Dario Vergara Viana <110841804+zarksoad@users.noreply.github.com>
2024-09-19 18:44:47 -0500

    feat: add courses page (#7)
    * addede all courses fetch
    
    * merge completed
    
    * change done

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,7 @@ export default async function HomePage() {
   const cookiesStore = cookies();
   const token = cookiesStore.get('AUTH_TOKEN');
 
-  const { failureRes, successRes } = await getStudentLastSeenClasses();
+  const { failureRes } = await getStudentLastSeenClasses();
   if (failureRes) return <p>{failureRes.detail}</p>;
 
   if (!token) return <p>Token not found</p>;
@@ -13,7 +13,6 @@ export default async function HomePage() {
   return (
     <main className="container mx-auto flex min-h-[100svh] items-center justify-center px-4">
       <h1>Bienvenido</h1>
-      <pre>{JSON.stringify(successRes, null, 2)}</pre>
     </main>
   );
 }
