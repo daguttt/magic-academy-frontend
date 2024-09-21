9d8c5ca0069abb7a7059859dc0aa4ca6e3b00b72
EmanuelOH <osorioemanuel0520@gmail.com>
2024-09-20 19:14:53 -0500

    Update card

--- src/components/last-course-seen/last-course-seen.tsx
+++ src/components/last-course-seen/last-course-seen.tsx
@@ -1,28 +1,50 @@
-import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+'use client'
 
-import { CirclePlay } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { getStudentLastSeenClasses } from '~/services/classes/last-seen-classes'; // Asegúrate de que la ruta sea correcta
 
-export default function LastCourseLetter() {
+interface LastSeenClass {
+  classId: number;
+  title: string;
+  classNumber: number;
+  totalCourseClasses: number;
+  courseId: number;
+}
+
+export default function YourComponent() {
+  const [lastSeenClasses, setLastSeenClasses] = useState<LastSeenClass[]>([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getStudentLastSeenClasses();
+      console.log(response); // Verifica la respuesta aquí
+
+      // Manejo de errores
+      if (response.failureRes) {
+        console.error("Error de la API:", response.failureRes);
+      } else if (response.successRes) {
+        // Verifica que sea un array
+        if (Array.isArray(response.successRes)) {
+          setLastSeenClasses(response.successRes);
+        } else {
+          console.error("La respuesta no es un array:", response.successRes);
+        }
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <Card className="group w-52 overflow-hidden rounded-b-lg border border-gray-200 shadow-lg">
-      <CardHeader className="relative p-0">
-        {/* TODO: Change this for a Link */}
-        <div>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKMNMx79cUuHHE-VQnycTJN5MfR7xaZ7hj6g&s"
-            alt="Profile Pic"
-            className="h-full w-full object-cover"
-          />
-          <div className="absolute inset-0 group-hover:bg-primary/30"></div>
-          <CirclePlay className="pointer-events-none absolute left-1/2 top-1/2 z-10 h-11 w-11 -translate-x-1/2 -translate-y-1/2 text-primary opacity-0 transition-opacity duration-300 group-hover:opacity-80"/>
-        </div>
-      </CardHeader>
-
-      <CardContent className="p-4">
-        <p className="text-xs text-gray-600">Class 1 of 20</p>
-        <CardTitle className="text-lg font-semibold">Title class</CardTitle>
-        <p className="text-sm text-gray-500">Title course</p>
-      </CardContent>
-    </Card>
+    <div>
+      <h1>Últimas Clases Vistas</h1>
+      <ul>
+        {lastSeenClasses.map(cls => (
+          <li key={cls.classId}>
+            {cls.title} (Clase {cls.classNumber} de {cls.totalCourseClasses})
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
