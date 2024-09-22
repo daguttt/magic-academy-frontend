0e8de2715b269046a5ec614b5859f39f34d32adc
daguttt <dagutmu667@gmail.com>
2024-09-21 20:20:12 -0500

    refactor: collocate logout button as it's used in the private layout

--- src/app/(private)/_components/log-out-button.tsx
+++ src/app/(private)/_components/log-out-button.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
-import { logoutAction } from '~/app/actions';
 import { Button } from '~/components/ui/button';
+import { logoutAction } from '../_actions/logout';
 
 export function LogoutButton() {
   const mutation = useMutation({ mutationFn: logoutAction });
