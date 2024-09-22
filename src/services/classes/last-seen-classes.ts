35befb28cfd9b88989515e0d7d3a603211149717
daguttt <dagutmu667@gmail.com>
2024-09-21 19:14:51 -0500

    feat: include flag to authenticate request to the API

--- src/services/classes/last-seen-classes.ts
+++ src/services/classes/last-seen-classes.ts
@@ -1,4 +1,3 @@
-import { cookies } from 'next/headers';
 import { z } from 'zod';
 
 import { fetchApi } from '~/lib/fetch-api';
@@ -24,16 +23,11 @@ export type LastSeenClassesResponseDto = z.infer<
 export async function getStudentLastSeenClasses(): Promise<
   ApiResponseDto<LastSeenClass[]>
 > {
-  const authToken = cookies().get('AUTH_TOKEN')?.value;
-  console.log(authToken,"at");
-
   const apiResponseDto = await fetchApi<LastSeenClassesResponseDto>({
+    isAuth: true,
     path: '/users/user/classes/in-progress/latest',
     init: {
       method: 'GET',
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
     },
     responseSchema: lastSeenClassesResponseSchema,
   });
