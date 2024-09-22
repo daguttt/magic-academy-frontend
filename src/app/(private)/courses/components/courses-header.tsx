4b22608c315a161452c524bf6ae40f4afb976080
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-09-22 18:40:37 -0500

    refactor: image

--- src/app/(private)/courses/components/courses-header.tsx
+++ src/app/(private)/courses/components/courses-header.tsx
@@ -4,14 +4,17 @@ import React from 'react';
 const CourseHeader: React.FC = () => {
   return (
     <header
-      className="bg-cover bg-center py-12 text-white"
+      className="relative bg-cover bg-center py-12 text-white"
       style={{
         backgroundImage:
           'url(https://www.criptonoticias.com/wp-content/uploads/2022/05/100-estudiantes-becados-aprender-programar-ethereum-1140x570.jpg)',
         height: '400px', // Ajusta la altura según lo necesites
       }}
     >
-      <div className="mx-auto max-w-4xl px-4">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      
+      <div className="relative mx-auto max-w-4xl px-4">
         <h1 className="text-4xl font-bold uppercase tracking-wide shadow-lg">
           ¡Accede a cientos de cursos gratis!
         </h1>
