ffa789bf9a538a50cb8790bff00b193d3bba4650
Vero Torrenegra <veronicatorrenegra@gmail.com>
2024-09-25 11:04:34 -0500

    fix: image logo in small viewport height screens

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import { CreateUserFormContainer } from './components/register-form-container';
 
 export default function Register() {
   return (
-    <main className="container mx-auto flex min-h-[100svh] items-center justify-center px-4">
+    <main className="sm-max-h:pt-28 sm-max-h:pb-6 container mx-auto flex min-h-[100svh] items-center justify-center px-4">
       <CreateUserFormContainer />
     </main>
   );
