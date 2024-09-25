aa6b3a95fbad35082f0c850ff266b140e987615e
Vero Torrenegra <veronicatorrenegra@gmail.com>
2024-09-25 12:26:44 -0500

    feat: add link to image logo

--- src/app/(private)/_components/navbar.tsx
+++ src/app/(private)/_components/navbar.tsx
@@ -1,31 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { Suspense } from 'react';
 
 import { Skeleton } from '~/components/ui/skeleton';
 import { NavbarUserAvatar } from './navbar-user-avatar';
 import { SidebarButton } from './sidebar-button';
-import Image from 'next/image';
 
 export async function Navbar() {
   return (
     <header className="content sticky flex h-16 items-center justify-between border-b">
       <SidebarButton />
 
-      <picture className="flex-1 pl-5">
-        <source
-          media="(prefers-color-scheme: light)"
-          srcSet="/img/larger-v-logo.png"
-        />
-        <source
-          media="(prefers-color-scheme: dark)"
-          srcSet="/img/larger-v-logo-white.png"
-        />
-        <Image
-          src="/img/larger-v-logo.png"
-          alt="Descripción de la imagen"
-          width={250}
-          height={250}
-        />
-      </picture>
+      <Link href="/home" className="flex h-full flex-1 items-center pl-5">
+        <picture>
+          <source
+            media="(prefers-color-scheme: light)"
+            srcSet="/img/larger-v-logo.png"
+          />
+          <source
+            media="(prefers-color-scheme: dark)"
+            srcSet="/img/larger-v-logo-white.png"
+          />
+          <Image
+            src="/img/larger-v-logo.png"
+            alt="Descripción de la imagen"
+            width={250}
+            height={250}
+          />
+        </picture>
+      </Link>
 
       {/* TODO: Add button to search courses for student  */}
       <Suspense fallback={<Skeleton className="h-20 w-36"></Skeleton>}>
