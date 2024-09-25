a2643c697cb2a2eb9bf8748e4f59758a6e2d985c
daguttt <dagutmu667@gmail.com>
2024-09-24 23:34:59 -0500

    refactor: format `navbar.tsx` file

--- src/app/(private)/_components/navbar.tsx
+++ src/app/(private)/_components/navbar.tsx
@@ -10,22 +10,22 @@ export async function Navbar() {
     <header className="content sticky flex h-16 items-center justify-between border-b">
       <SidebarButton />
 
-      <picture className='flex-1 pl-5'>
-              <source
-                media="(prefers-color-scheme: light)"
-                srcSet="/img/larger-v-logo.png"
-              />
-              <source
-                media="(prefers-color-scheme: dark)"
-                srcSet="/img/larger-v-logo-white.png"
-              />
-              <Image
-                src="/img/larger-v-logo.png"
-                alt="Descripción de la imagen"
-                width={250}
-                height={250}
-              />
-            </picture>
+      <picture className="flex-1 pl-5">
+        <source
+          media="(prefers-color-scheme: light)"
+          srcSet="/img/larger-v-logo.png"
+        />
+        <source
+          media="(prefers-color-scheme: dark)"
+          srcSet="/img/larger-v-logo-white.png"
+        />
+        <Image
+          src="/img/larger-v-logo.png"
+          alt="Descripción de la imagen"
+          width={250}
+          height={250}
+        />
+      </picture>
 
       {/* TODO: Add button to search courses for student  */}
       <Suspense fallback={<Skeleton className="h-20 w-36"></Skeleton>}>
